178208b30102c2e5c66a3d0c70c466e21339bca8
ynwahid <yusufnurwahid.contact@gmail.com>
2022-04-14 06:51:50 +0700

    app say hello done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ynwahid/app-say-hello
+
+go 1.18
+
+require github.com/ynwahid/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/ynwahid/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Yusuf"))
+}
